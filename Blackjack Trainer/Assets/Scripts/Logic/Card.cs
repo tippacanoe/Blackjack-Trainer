3025e10e99ae72cd58ee451c307c757559230e61
Logan <loganjm0380@gmail.com>
2025-10-26 11:56:12 -0600

    Sprites added to cards

--- Blackjack Trainer/Assets/Scripts/Logic/Card.cs
+++ Blackjack Trainer/Assets/Scripts/Logic/Card.cs
@@ -6,10 +6,16 @@ public class Card : ScriptableObject
 {
     [SerializeField] private string suit;
     [SerializeField] private int rank;
+    [SerializeField] private Sprite sprite;
     
     public int getRank()
     {
         return rank;
     } // getRank
+
+    public Sprite getSprite()
+    {
+        return sprite;
+    } // getSprite
 
 } // Card
